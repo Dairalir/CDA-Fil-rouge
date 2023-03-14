38e68b2a9bcf409f63db0703e2fdc92f952f7356
Dairalir <daimanvm@gmail.com>
2023-03-14 16:49:42 +0100

    requete filrouge

--- BDD/Requete/requete.sql
+++ BDD/Requete/requete.sql
@@ -1,3 +1,13 @@
 SELECT SUM(cmd_qte*pro_prixht) AS CA , cmd_date
-FROM produit
-JOIN Commande ON produit.pro_id = commande.pro_id
+FROM Produit
+JOIN Commande ON Produit.pro_id = Commande.pro_id
+WHERE YEAR(cmd_date) = '2023'
+GROUP BY MONTH(cmd_date)
+;
+
+SELECT SUM(cmd_qte*pro_prixht) AS CA , fou_nom
+FROM Produit
+JOIN Commande ON Produit.pro_id = Commande.pro_id
+JOIN Fournisseur ON Produit.fou_id = Fournisseur.fou_id
+GROUP BY fou_nom
+;
